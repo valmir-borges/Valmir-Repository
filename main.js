f8cd265c553ce4f8587aa05ab8c7bb1ef2315207
valmir-borges <129890080+valmir-borges@users.noreply.github.com>
2023-04-05 11:27:56 -0300

    alteração js

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 document.addEventListener("click" , showcolor)
 
+document.getElementById("botao").addEventListener("click", exibenome)
+
 function showcolor ()
 {
     document.body.style.backgroundColor = "red";
+}
+
+function exibenome ()
+{
+    let usuario = document.getElementById("nome").value;
+    document.getElementById("mensagem").innerText = "Meu nome é: " + usuario;
+    document.getElementById("nome").value = "";
 }
