6ead5d883dc6d45e1839a460d7ea973770a3db2c
valmir-borges <129890080+valmir-borges@users.noreply.github.com>
2023-04-05 11:29:34 -0300

    alteração js

--- main.js
+++ main.js
@@ -10,6 +10,6 @@ function showcolor ()
 function exibenome ()
 {
     let usuario = document.getElementById("nome").value;
-    document.getElementById("mensagem").innerText = "Meu nome é: " + usuario;
+    document.getElementById("mensagem").innerHTML = "Meu nome é: " + usuario;
     document.getElementById("nome").value = "";
 }
