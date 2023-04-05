230427a7499b3f5208d62248c4177974cc18530a
valmir-borges <129890080+valmir-borges@users.noreply.github.com>
2023-04-05 11:48:11 -0300

    alteração js

--- main.js
+++ main.js
@@ -13,3 +13,14 @@ function exibenome ()
     document.getElementById("mensagem").innerHTML = "Meu nome é: " + usuario;
     document.getElementById("nome").value = "";
 }
+
+document.getElementById("botao-de-soma").addEventListener("click", somar)
+
+function somar ()
+{
+    let num1 = document.getElementById("num1").value;
+    let num2 = document.getElementById("num2").value;
+    let num3 = num1 + num2
+    document.getElementById("resultado").innerHTML = "Resultado: " + num3
+
+}
