f2d019021e552024f94c7649fa9d779d656841a0
valmir-borges <129890080+valmir-borges@users.noreply.github.com>
2023-04-05 12:24:14 -0300

    alteração js

--- main.js
+++ main.js
@@ -32,7 +32,7 @@ function sub ()
     let num4 = document.getElementById("num3").value;
     let num5 = document.getElementById("num4").value;
     let num6 = parseFloat(num4) - parseFloat(num5);
-    document.getElementById("resultado").innerHTML = "Resultado: " + num6;
+    document.getElementById("resultado1").innerHTML = "Resultado: " + num6;
 
 }
 
@@ -43,7 +43,7 @@ function mult ()
     let num7 = document.getElementById("num5").value;
     let num8 = document.getElementById("num6").value;
     let num9 = parseFloat(num7) * parseFloat(num8);
-    document.getElementById("resultado").innerHTML = "Resultado: " + num9;
+    document.getElementById("resultado2").innerHTML = "Resultado: " + num9;
 
 }
 
@@ -54,6 +54,6 @@ function div ()
     let num10 = document.getElementById("num7").value;
     let num11 = document.getElementById("num8").value;
     let num12 = parseFloat(num7) / parseFloat(num8);
-    document.getElementById("resultado").innerHTML = "Resultado: " + num12;
+    document.getElementById("resultado3").innerHTML = "Resultado: " + num12;
 
 }
